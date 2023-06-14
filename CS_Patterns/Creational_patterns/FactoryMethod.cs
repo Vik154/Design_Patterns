b13d2dea15aa3c54a8fa8a90f5fb5669fcea482d
Vik154 <Smorodin_1209@mail.ru>
2023-06-14 13:56:33 +0700

    Создание паттерна "фабричный метод"

---
+++ CS_Patterns/Creational_patterns/FactoryMethod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Patterns.Creational_patterns {
+    internal class FactoryMethod {
+    }
+}
