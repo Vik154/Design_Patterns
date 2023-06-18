0aef999304511386f2dd8240f220bb2c61c7e821
Vik154 <Smorodin_1209@mail.ru>
2023-06-18 07:41:59 +0700

    Реализация паттерна "Прототип" (Prototype)

--- Python_Patterns/Program.py
+++ Python_Patterns/Program.py
@@ -1,4 +1,5 @@
-﻿import FactoryMethod
+﻿from Creational_patterns.Prototype import test_prototype
+import FactoryMethod
 from Creational_patterns.AbstractFactory import *
 from Creational_patterns.Singleton import test_singleton
 
@@ -15,16 +16,18 @@ class Handler():
         \n1 - Паттерн \"Фабричный метод\" \
         \n2 - Паттерн \"Абстрактнрая фабрика\" \
         \n3 - Паттерн \"Singleton\" \
+        \n4 - Паттерн \"Прототип\" \
         \n";
 
         while (True):
             key = input(info)
             match key:
-                case 'q':
-                    return
+                case 'q': return;
+                case 'й': return;
                 case '1': self._factory_method.show(); continue;
                 case '2': self._abstract_factory.show(); continue;
                 case '3': test_singleton(); continue;
+                case '4': test_prototype(); continue;
                 case _:
                     print("Не тыкай чё-попало")
                 
