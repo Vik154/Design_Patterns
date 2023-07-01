f5ebc3dc6cbbcfd9ed48a3a721b19b24d9dded0b
Vik154 <Smorodin_1209@mail.ru>
2023-07-01 19:35:46 +0700

    Test branches

---
+++ CS_Patterns/TEST.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace TESTING;
+
+static class TestCommit {
+    public static void test_commit() {
+
+        foreach (var item in "string") {
+            Console.WriteLine($"char: {item}");
+        }
+    }
+}
