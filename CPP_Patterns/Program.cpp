8016c3ef2513bcebcee4d7a4cd3cafeca0af7d00
Vik154 <Smorodin_1209@mail.ru>
2023-07-13 17:32:36 +0700

    program cpp

--- CPP_Patterns/Program.cpp
+++ CPP_Patterns/Program.cpp
@@ -20,68 +20,64 @@ namespace Pattern {
 	using std::cout;
 	using std::cin;
 
-	class Handler final {
-	public:
-		Handler() = default;
 
-		void run() {
-			string result;
-			string info = "\nВыберите команду:"
-				"\nq - Выход"
-				"\n1 - Паттерн \"Фабричный метод\""
-				"\n2 - Паттерн \"Абстракная фабрика\""
-				"\n3 - Паттерн \"Одиночка\""
-				"\n4 - Паттерн \"Прототип\""
-				"\n5 - Паттерн \"Строитель\""
-				"\n6 - Паттерн \"Стратегия\""
-				"\n7 - Паттерн \"Наблюдатель\""
-				"\n8 - Паттерн \"Команда\""
-				"\n9 - Паттерн \"Шаблонный метод\""
-				"\n10 - Паттерн \"Итератор\""
-				"\n11 - Паттерн \"Состояние\""
-				"\n12 - Паттерн \"Цепочка обязанностей\""
-				"\n";
+	void run() {
+		string result;
+		string info = "\nВыберите команду:"
+			"\nq - Выход"
+			"\n1 - Паттерн \"Фабричный метод\""
+			"\n2 - Паттерн \"Абстракная фабрика\""
+			"\n3 - Паттерн \"Одиночка\""
+			"\n4 - Паттерн \"Прототип\""
+			"\n5 - Паттерн \"Строитель\""
+			"\n6 - Паттерн \"Стратегия\""
+			"\n7 - Паттерн \"Наблюдатель\""
+			"\n8 - Паттерн \"Команда\""
+			"\n9 - Паттерн \"Шаблонный метод\""
+			"\n10 - Паттерн \"Итератор\""
+			"\n11 - Паттерн \"Состояние\""
+			"\n12 - Паттерн \"Цепочка обязанностей\""
+			"\n13 - Паттерн \"Интерпретатор\""
+			"\n";
 
-			while (true) {
-				cout << info;
-				cin >> result;
+		while (true) {
+			cout << info;
+			cin >> result;
 
-				try {
-					if (result == "q") return;
-					int res = stoi(result); }
-				catch (...) {
-					cout << "Чё ты тыкаешь чё-попало. Такой команды нет, не тыкай!";
-					cin.clear();
-					continue;
-				}
+			try {
+				if (result == "q") return;
+				int res = stoi(result);
+			}
+			catch (...) {
+				cout << "Чё ты тыкаешь чё-попало. Такой команды нет, не тыкай!";
+				cin.clear();
+				continue;
+			}
 
-				switch (stoi(result)) {
-					case 1: Creational::test_factory_method();    break;
-					case 2: Creational::test_abstract_factory();  break;
-					case 3: Creational::test_singleton();         break;
-					case 4: Creational::test_prototype();         break;
-					case 5: Creational::test_builder();           break;
-					case 6: Behavioral::test_strategey();         break;
-					case 7: Behavioral::test_observer();          break;
-					case 8: Behavioral::test_command();           break;
-					case 9: Behavioral::test_template();          break;
-					case 10: Behavioral::test_iterator();         break;
-					case 11: Behavioral::test_state();            break;
-					case 12: Behavioral::test_chain();            break;
-					case 13: Behavioral::test_interpreter();      break;
-					default: cin.clear();                         break;
-				}
+			switch (stoi(result)) {
+			case 1: Creational::test_factory_method();    break;
+			case 2: Creational::test_abstract_factory();  break;
+			case 3: Creational::test_singleton();         break;
+			case 4: Creational::test_prototype();         break;
+			case 5: Creational::test_builder();           break;
+			case 6: Behavioral::test_strategey();         break;
+			case 7: Behavioral::test_observer();          break;
+			case 8: Behavioral::test_command();           break;
+			case 9: Behavioral::test_template();          break;
+			case 10: Behavioral::test_iterator();         break;
+			case 11: Behavioral::test_state();            break;
+			case 12: Behavioral::test_chain();            break;
+			case 13: Behavioral::test_interpreter();      break;
+			default: cin.clear();                         break;
 			}
 		}
-	};
+	}
 }
 
+// ---------------------------------------------------------------
 int main(int argc, char** argv) {
-	using namespace Pattern;
 	setlocale(0, "");
-
-	unique_ptr<Handler> handler = make_unique<Handler>();
-	handler->run();
-
+	Pattern::run();
 	return 0;
 }
+// ---------------------------------------------------------------
